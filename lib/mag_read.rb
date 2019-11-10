9a169b198cdbf959c18da12d688ec673ae00e74b
weshatheleopard <github@wesha.name>
2019-11-10 08:33:14 -0600

    Adding some constants, separating TapeSplitter

--- lib/mag_read.rb
+++ lib/mag_read.rb
@@ -1,15 +1,17 @@
 require 'wavefile'
+require 'waveform_fixer'
+require 'tape_splitter'
+
 require 'term/ansicolor'
 include Term::ANSIColor
 
 class MagRead
   include WaveformFixer
+  include TapeSplitter
 
   CUTOFF_COEFF = 1.6   # Impulses longer than (base impulse length * CUTOFF_COEFF) will be
                        # considered to encode "1"s; shorter - "0"s.
   BIT_TOO_LONG = 2.8   # No bit impulse should be this long. Must be file format issue.
-
-  DISCREPANCY = 6      # Allowed number of samples difference in pilot squence
 
   attr_accessor :bk_file
 
@@ -210,65 +212,6 @@ class MagRead
     end
     @marker_counter +=1
     return false
-  end
-
-  # Split a wingle wav file containing the entire tape into multiple
-  # WAV files each containing and individual BK file recording.
-  def split_tape
-    convert_file_to_legths if @wavelengths_hash.nil?
-
-    prev_len = 0
-    counter = 0
-
-    start_pos_candidate = 0
-    split_locations = [ ]
-
-    @wavelengths_hash.each_pair { |position, len|
-      # If this bit is about as long as the previous one, this can be the header pilot sequence.
-      # Let's count how long it is.
-      if (len - prev_len).abs < DISCREPANCY then
-        counter += 1
-      else
-        # Discrepancy too large. Let's see if it's an accident or an actual start marker.
-        if (counter > 0o5000) && (len > (3.5 * prev_len)) then  # So it is an actual start marker.
-          split_locations << start_pos_candidate
-        end  # Just an accident. Start over.
-
-        counter = 0
-        start_pos_candidate = position
-      end
-
-      prev_len = len
-    }
-
-    split_locations << start_pos_candidate
-
-    # Now proceed to actually split the file
-
-    position_in_file = 0
-    piece_start_location  = 0
-    current_write_buffer = WaveFile::Buffer.new([], WaveFile::Format.new(:mono, :pcm_16, 44100))
-
-    reader = WaveFile::Reader.new(@filename, WaveFile::Format.new(:mono, :pcm_16, 44100))
-    reader.each_buffer(1024*1024) do |buffer|
-      debug 10, "Loaded #{buffer.samples.length.to_s.bold} samples"
-
-      buffer.samples.each { |c|
-        current_write_buffer.samples << c
-        position_in_file += 1
-
-        if split_locations.include?(position_in_file) then
-          file_name = "#{('0000000000' + piece_start_location.to_s)[-10..-1]}-#{('0000000000' + position_in_file.to_s)[-10..-1]}.wav"
-          debug 5, "Saving: #{file_name}"
-          WaveFile::Writer.new(file_name, WaveFile::Format.new(:mono, :pcm_16, 44100)) { |writer| writer.write(current_write_buffer) }
-
-          # Start collectiong a new file
-          piece_start_location = position_in_file
-          current_write_buffer = WaveFile::Buffer.new([], WaveFile::Format.new(:mono, :pcm_16, 44100))
-        end
-      }
-    end
-
   end
 
 end
---
+++ lib/tape_splitter.rb
@@ -0,0 +1,72 @@
+require 'wavefile'
+
+require 'term/ansicolor'
+include Term::ANSIColor
+
+module TapeSplitter
+  DISCREPANCY = 6      # Allowed number of samples difference in pilot squence
+
+  # Split a wingle wav file containing the entire tape into multiple
+  # WAV files each containing and individual BK file recording.
+  def split_tape
+    convert_file_to_legths if @wavelengths_hash.nil?
+
+    prev_len = 0
+    counter = 0
+
+    start_pos_candidate = 0
+    split_locations = [ ]
+
+    @wavelengths_hash.each_pair { |position, len|
+      # If this bit is about as long as the previous one, this can be the header pilot sequence.
+      # Let's count how long it is.
+      if (len - prev_len).abs < DISCREPANCY then
+        counter += 1
+      else
+        # Discrepancy too large. Let's see if it's an accident or an actual start marker.
+        if (counter > 0o5000) && (len > (3.5 * prev_len)) then  # So it is an actual start marker.
+          split_locations << start_pos_candidate
+        end  # Just an accident. Start over.
+
+        counter = 0
+        start_pos_candidate = position
+      end
+
+      prev_len = len
+    }
+
+    split_locations << start_pos_candidate
+
+    # Now proceed to actually split the file
+
+    position_in_file = 0
+    piece_start_location  = 0
+    current_write_buffer = make_write_buffer()
+
+    reader = WaveFile::Reader.new(@filename, WaveFile::Format.new(:mono, :pcm_16, 44100))
+    reader.each_buffer(1024*1024) do |buffer|
+      debug 10, "Loaded #{buffer.samples.length.to_s.bold} samples"
+
+      buffer.samples.each { |c|
+        current_write_buffer.samples << c
+        position_in_file += 1
+
+        if split_locations.include?(position_in_file) then
+          file_name = "#{('0000000000' + piece_start_location.to_s)[-10..-1]}-#{('0000000000' + position_in_file.to_s)[-10..-1]}.wav"
+          debug 5, "Saving: #{file_name}"
+          WaveFile::Writer.new(file_name, WaveFile::Format.new(:mono, :pcm_16, 44100)) { |writer| writer.write(current_write_buffer) }
+
+          # Start collectiong a new file
+          piece_start_location = position_in_file
+          current_write_buffer = make_write_buffer()
+        end
+      }
+    end
+
+  end
+
+  def make_write_buffer()
+    WaveFile::Buffer.new([], WaveFile::Format.new(:mono, :pcm_16, 44100))
+  end
+
+end
