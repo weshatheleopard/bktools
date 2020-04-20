0205dece9e52e0b39c8eea49af825f51573d7c10
weshatheleopard <github@wesha.name>
2020-04-19 21:09:03 -0500

    More KryoFlux code

--- lib/fdd_reader.rb
+++ lib/fdd_reader.rb
@@ -9,32 +9,41 @@ class FddReader
     @debuglevel = debuglevel
     @filename = filename
     @stream = nil
-    @pos = 0
+    @file_position = 0
+    @stream_position = 0
   end
 
   def read_track
     File.open(@filename, "rb") { |f| @stream = f.read.bytes }
 
     loop do
-    
-      block_type = @stream[@pos]
-      @pos += 1
+
+      block_type = @stream[@file_position]
+      @file_position += 1
 
       case block_type
       when 0x00..0x07 then
-        lsb = @stream[@pos]
-        @pos += 1
+        lsb = @stream[@file_position]
+        @file_position += 1
+        @stream_position += 2
 
         flux_val = (block_type << 8) + lsb;
-        debug(20) { puts "Flux2 block: #{flux_val}" }
-      when 0x08 then nil # NOP1 block, skip 1 byte total
-      when 0x09 then @pos += 1 # NOP2 block, skip 2 bytes total
-      when 0x0A then @pos += 2 # NOP3 block, skip 3 bytes total
+        debug(20) { "Flux2 block: #{flux_val}" }
+      when 0x08 then # NOP1 block, skip 1 byte
+        @stream_position += 1
+      when 0x09 then # NOP2 block, skip 2 bytes
+        @stream_position += 2
+        @file_position += 1
+      when 0x0A then # NOP3 block, skip 3 bytes
+        @stream_position += 3
+        @file_position += 2
       when 0x0B then
-        raise "OVL16 currently not supported"
-      when 0x0D then 
+        raise "OVL16 block currently not supported"
+      when 0x0D then
         break if out_of_stream_block
       when 0x0E..0xFF then
+        @stream_position += 1
+
         debug(20) { "Flux1 block: #{block_type}" }
       else
         raise "Unknown block type: %02x" % [block_type]
@@ -44,51 +53,58 @@ class FddReader
   end
 
   def out_of_stream_block
-    puts "* Out-of-stream Block:"
-    oob_block_type = @stream[@pos]
-    @pos +=1
+    oob_block_type = @stream[@file_position]
+    @file_position +=1
     sz = read_word
 
     case oob_block_type
     when 0x00 then
       raise "Invalid OOB"
-    when 0x01 then puts "Stream Information (multiple per track)---"
+    when 0x01 then
       raise "Protocol error" if sz != 8
-   
+
       stream_position = read_dword
       transfer_time = read_dword
-puts stream_position, transfer_time
 
-
-    when 0x02 then puts "Index signal data"
+      debug(10) {
+        "Stream Information".yellow +
+          "\n  * KryoFlux stream position : " + stream_position.to_s.bold +
+          "\n  * Reader stream position   : " + @stream_position.to_s.bold +
+          "\n  * Transfer time (ms)       : " + transfer_time.to_s.bold
+      }
+    when 0x02 then
       raise "Protocol error" if sz != 12
-   
+
       stream_position = read_dword
       sample_counter = read_dword
       index_counter = read_dword
 
-puts stream_position, sample_counter, index_counter
-
-
-    when 0x03 then 
-      puts "StreamEnd - No more flux to transfer (one per track)"
+      debug(10) {
+        "Index signal data".blue.bold +
+          "\n  * Next KryoFlux stream position  : " + stream_position.to_s.bold +
+          "\n  * Sample Counter since last flux : " + sample_counter.to_s.bold +
+          "\n  * Index Clock valie              : " + index_counter.to_s.bold
+      }
+    when 0x03 then
       raise "Protocol error" if sz != 8
-
       stream_position = read_dword
       result_code = read_dword
 
-puts stream_position, result_code
-
+      debug(5) {
+        "Stream End".red +
+          "\n  * KryoFlux stream position : " + stream_position.to_s.bold +
+          "\n  * Return code              : " + result_code.to_s.bold
+      }
     when 0x04 then
       str = ""
       sz.times {
-        str << @stream[@pos].chr
-        @pos += 1
+        str << @stream[@file_position].chr
+        @file_position += 1
       }
 
       puts "   KryoFlux device info: [#{str}] size=#{sz}"
     when 0x0D then
-      puts "EOF --  (no more data to process)"
+      debug(5) { "End of file".red }
       return true
     else raise
     end
@@ -96,20 +112,20 @@ puts stream_position, result_code
   end
 
   def read_word
-    word = Tools::bytes2word(@stream[@pos], @stream[@pos+1])
-    @pos += 2
+    word = Tools::bytes2word(@stream[@file_position], @stream[@file_position+1])
+    @file_position += 2
     word
   end
 
   def read_dword
-    dword = @stream[@pos] |
-             (@stream[@pos+1] << 8) |
-             (@stream[@pos+2] << 16) |
-             (@stream[@pos+3] << 24)
-    @pos += 4
+    dword = @stream[@file_position] |
+             (@stream[@file_position+1] << 8) |
+             (@stream[@file_position+2] << 16) |
+             (@stream[@file_position+3] << 24)
+    @file_position += 4
     dword
   end
-  
+
   def debug(msg_level)
     return if msg_level > @debuglevel
     msg = yield
@@ -118,7 +134,4 @@ puts stream_position, result_code
 
 end
 
-=begin
-f = FddReader.new('trk00.0.raw')
-f.read
-=end
+# f = FddReader.new('trk00.0.raw'); f.read
