ddf7736f9c536565136c1fcfe00c10525cb7e469
weshatheleopard <github@wesha.name>
2022-07-16 05:46:44 +0000

    Clean up disassembler code

--- lib/disassembler.rb
+++ lib/disassembler.rb
@@ -2,7 +2,7 @@ module Disassembler
   # PDP-11 handbook: http://gordonbell.azurewebsites.net/digital/pdp%2011%20handbook%201969.pdf
   # Written with heavy inflience from https://github.com/caldwell/pdp11dasm
 
-  REGISTER_NAMES = %w{ R0 R1 R2 R3 R4 R5 SP PC }
+  REGISTER_NAMES = %w[ R0 R1 R2 R3 R4 R5 SP PC ]
 
   def disassemble_with_labels(predefined_labels = {})
     # TODO: populate @labels and @references from predefined_labels
@@ -29,25 +29,25 @@ module Disassembler
     # TODO: distinguish between local and global labels
     @current_offset = 0
 
-    @cmp_step = 0
+    @previous_command = ''
 
     while @current_offset < body.length do
       oct = ''
       start_offset = @current_offset
       @acceptable_label_addresses << (start_address + @current_offset) if pass == 1
-      cmd, step, newline, label = decode
+      cmd, step, add_newline, label = decode
 
       next unless str
 
       case step
-      when 2 then oct = Tools::octal(word_at(start_offset)) + "              "
-      when 4 then oct = Tools::octal(word_at(start_offset)) + " " + Tools::octal(word_at(start_offset + 2)) + "       "
-      when 6 then oct = Tools::octal(word_at(start_offset)) + " " + Tools::octal(word_at(start_offset + 2)) + " " + Tools::octal(word_at(start_offset + 4))
+      when 2 then oct = Tools::octal(word_at(start_offset)) + '              '
+      when 4 then oct = Tools::octal(word_at(start_offset)) + ' ' + Tools::octal(word_at(start_offset + 2)) + '       '
+      when 6 then oct = Tools::octal(word_at(start_offset)) + ' ' + Tools::octal(word_at(start_offset + 2)) + ' ' + Tools::octal(word_at(start_offset + 4))
       end
 
       label += ':' unless label.nil? || (label == '')
       str += "#{Tools::octal(start_address + start_offset)}: #{oct}\t#{label}\t#{cmd}\n"
-      str += "\n" if newline
+      str += "\n" if add_newline
     end
 
     str
@@ -63,13 +63,7 @@ module Disassembler
   #  * Whether a newline should follow the command;
   #  * Label that should precede the command.
   def decode
-    if @cmp_step == 1 then
-      @cmp_step = 2
-    else
-      @cmp_step = 0
-    end
-
-    newline = false
+    add_newline = false
     command_start_offset = @current_offset
     current_word = Tools::bytes2word(body[@current_offset], body[@current_offset + 1])
 
@@ -81,135 +75,131 @@ module Disassembler
         case (current_word & 0o177700)
         when 0o000000 then
           case (current_word)
-          when 0 then cmd = "HALT"
-          when 1 then cmd = "WAIT"
-          when 2 then cmd = "RTI"
-          when 3 then cmd = "BPT"
-          when 4 then cmd = "IOT"
-          when 5 then cmd = "RESET"
-          when 6 then cmd = "RTT"
+          when 0 then cmd = 'HALT'
+          when 1 then cmd = 'WAIT'
+          when 2 then cmd = 'RTI'
+          when 3 then cmd = 'BPT'
+          when 4 then cmd = 'IOT'
+          when 5 then cmd = 'RESET'
+          when 6 then cmd = 'RTT'
           end
         when 0o000100 then
-          newline = true
-          cmd = "JMP\t" + parse_operand(current_word & 0o77)
+          add_newline = true
+          cmd = dd('JMP', current_word)
         when 0o000200 then
           case (current_word)
-          when 0o200, 0o201, 0o202, 0o203, 0o204, 0o205, 0o206
-            then
-              newline = true
-              cmd = "RTS\t" + parse_operand(current_word & 0o7)
-          when 0o207
-            then
-              newline = true
-              cmd = "RET"
-          when 0o240 then cmd = "NOP"
-          when 0o241 then cmd = "CLC"
-          when 0o242 then cmd = "CLV"
-          when 0o244 then cmd = "CLZ"
-          when 0o250 then cmd = "CLN"
-          when 0o257 then cmd = "CCC"
-          when 0o261 then cmd = "SEC"
-          when 0o262 then cmd = "SEV"
-          when 0o264 then cmd = "SEZ"
-          when 0o270 then cmd = "SEN"
-          when 0o277 then cmd = "SCC"
+          when 0o200, 0o201, 0o202, 0o203, 0o204, 0o205, 0o206 then
+            add_newline = true
+            cmd = "RTS\t" + parse_operand(current_word & 0o7)
+          when 0o207 then
+            add_newline = true
+            cmd = 'RET'
+          when 0o240 then cmd = 'NOP'
+          when 0o241 then cmd = 'CLC'
+          when 0o242 then cmd = 'CLV'
+          when 0o244 then cmd = 'CLZ'
+          when 0o250 then cmd = 'CLN'
+          when 0o257 then cmd = 'CCC'
+          when 0o261 then cmd = 'SEC'
+          when 0o262 then cmd = 'SEV'
+          when 0o264 then cmd = 'SEZ'
+          when 0o270 then cmd = 'SEN'
+          when 0o277 then cmd = 'SCC'
           end
         when 0o000300 then
-          cmd = "SWAB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('SWAB', current_word)
         when 0o000400, 0o000500, 0o000600, 0o000700 then
-          newline = true
-          cmd = "BR\t" + address_or_label(offset2address(current_word & 0o377))
+          add_newline = true
+          cmd = off('BR', current_word)
         end
       when 0o001000 then
         case (current_word & 0o177400)
         when 0o001000 then
-          cmd = "BNE\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BNE', current_word)
         when 0o001400 then
-          cmd = "BEQ\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BEQ', current_word)
         end
       when 0o002000 then
         case (current_word & 0o177400)
         when 0o002000 then
-          cmd = "BGE\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BGE', current_word)
         when 0o002400 then
-          cmd = "BLT\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BLT', current_word)
         end
       when 0o003000 then
         case (current_word & 0o177400)
         when 0o003000 then
-          cmd = "BGT\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BGT', current_word)
         when 0o003400 then
-          cmd = "BLE\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BLE', current_word)
         end
       when 0o004000 then # JSR - 004RDD
         r = (current_word >> 6) & 0o7
 
         if r == 7 then
-          cmd = "CALL\t" + parse_operand(current_word & 0o77)
+          cmd = dd('CALL', current_word)
         else
-          cmd = "JSR\t" + parse_operand(r) + "," + parse_operand(current_word & 0o77)
+          cmd = rdd('JSR', current_word)
         end
       when 0o005000 then
         case (current_word & 0o177700)
         when 0o005000 then
-          cmd = "CLR\t" + parse_operand(current_word & 0o77)
+          cmd = dd('CLR', current_word)
         when 0o005100 then
-          cmd = "COM\t" + parse_operand(current_word & 0o77)
+          cmd = dd('COM', current_word)
         when 0o005200 then
-          cmd = "INC\t" + parse_operand(current_word & 0o77)
+          cmd = dd('INC', current_word)
         when 0o005300 then
-          cmd = "DEC\t" + parse_operand(current_word & 0o77)
+          cmd = dd('DEC', current_word)
         when 0o005400 then
-          cmd = "NEG\t" + parse_operand(current_word & 0o77)
+          cmd = dd('NEG', current_word)
         when 0o005500 then
-          cmd = "ADC\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ADC', current_word)
         when 0o005600 then
-          cmd = "SBC\t" + parse_operand(current_word & 0o77)
+          cmd = dd('SBC', current_word)
         when 0o005700 then
-          cmd = "TST\t" + parse_operand(current_word & 0o77)
+          cmd = dd('TST', current_word)
         end
       when 0o006000 then
         case (current_word & 0o177700)
         when 0o006000 then
-          cmd = "ROR\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ROR', current_word)
         when 0o006100 then
-          cmd = "ROL\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ROL', current_word)
         when 0o006200 then
-          cmd = "ASR\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ASR', current_word)
         when 0o006300 then
-          cmd = "ASL\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ASL', current_word)
         when 0o006400 then
           cmd = "MARK\t" + (current_word & 0o77).to_s(8)
         when 0o006500 then
-          cmd = "MFPI\t" + parse_operand(current_word & 0o77)
+          cmd = dd('MFPI', current_word)
         when 0o006600 then
-          cmd = "MTPI\t" + parse_operand(current_word & 0o77)
+          cmd = dd('MTPI', current_word)
         when 0o006700 then
-          cmd = "SXT\t" + parse_operand(current_word & 0o77)
+          cmd = dd('SXT', current_word)
         end
       when 0o007000 then
         # Not used
       end
     when 0o010000 then
-      cmd = "MOV\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('MOV', current_word)
     when 0o020000 then
-      @cmp_step = 1
-      cmd = "CMP\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('CMP', current_word)
     when 0o030000 then
-      cmd = "BIT\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('BIT', current_word)
     when 0o040000 then
-      cmd = "BIC\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('BIC', current_word)
     when 0o050000 then
-      cmd = "BIS\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('BIS', current_word)
     when 0o060000 then
-      cmd = "ADD\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('ADD', current_word)
     when 0o070000 then
       case (current_word & 0o177000)
       when 0o074000 then
-        address = (start_address + @current_offset + 2 - ((current_word & 0o77) * 2))
-        cmd = "XOR\t" + parse_operand((current_word >> 6) & 0o7) + ',' + parse_operand(current_word & 0o77)
+        cmd = rdd('XOR', current_word)
       when 0o077000 then
-        newline = true
+        add_newline = true
         address = (start_address + @current_offset + 2 - ((current_word & 0o77) * 2))
         cmd = "SOB\t" + parse_operand((current_word >> 6) & 0o7) + ',' + address_or_label(address)
       end
@@ -218,39 +208,37 @@ module Disassembler
       when 0o100000 then
         case (current_word & 0o177400)
         when 0o100000 then
-          cmd = "BPL\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BPL', current_word)
         when 0o100400 then
-          cmd = "BMI\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BMI', current_word)
         end
       when 0o101000 then
         case (current_word & 0o177400)
         when 0o101000 then
-          cmd = "BHI\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BHI', current_word)
         when 0o101400 then #
-          cmd = "BLOS\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BLOS', current_word)
         end
       when 0o102000 then
         case (current_word & 0o177400)
         when 0o102000 then
-          cmd = "BVC\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BVC', current_word)
         when 0o102400 then
-          cmd = "BVS\t" + address_or_label(offset2address(current_word & 0o377))
+          cmd = off('BVS', current_word)
         end
       when 0o103000 then
         case (current_word & 0o177400)
         when 0o103000 then
-          if @cmp_step == 2 then # Immediately after comparison, BHIS makes better sense than BCC
-            @cmp_step = 0
-            cmd = "BHIS\t" + address_or_label(offset2address(current_word & 0o377))
+          if @previous_command =~ /^CMP/i then # Immediately after comparison, BHIS makes better sense than BCC
+            cmd = off('BHIS', current_word)
           else
-            cmd = "BCC\t" + address_or_label(offset2address(current_word & 0o377))
+            cmd = off('BCC', current_word)
           end
         when 0o103400 then
-          if @cmp_step == 2 then # Immediately after comparison, BLO makes better sense than BCS
-            @cmp_step = 0
-            cmd = "BLO\t" + address_or_label(offset2address(current_word & 0o377))
+          if @previous_command =~ /^CMP/i then # Immediately after comparison, BLO makes better sense than BCS
+            cmd = off('BLO', current_word)
           else
-            cmd = "BCS\t" + address_or_label(offset2address(current_word & 0o377))
+            cmd = off('BCS', current_word)
           end
         end
       when 0o104000 then
@@ -264,60 +252,77 @@ module Disassembler
       when 0o105000 then
         case (current_word & 0o177700)
         when 0o105000 then
-          cmd = "CLRB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('CLRB', current_word)
         when 0o105100 then
-          cmd = "COMB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('COMB', current_word)
         when 0o105200 then
-          cmd = "INCB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('INCB', current_word)
         when 0o105300 then
-          cmd = "DECB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('DECB', current_word)
         when 0o105400 then
-          cmd = "NEGB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('NEGB', current_word)
         when 0o105500 then
-          cmd = "ADCB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ADCB', current_word)
         when 0o105600 then
-          cmd = "SBCB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('SBCB', current_word)
         when 0o105700 then
-          cmd = "TSTB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('TSTB', current_word)
         end
       when 0o106000 then
         case (current_word & 0o177700)
         when 0o106000 then
-          cmd = "RORB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('RORB', current_word)
         when 0o106100 then
-          cmd = "ROLB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ROLB', current_word)
         when 0o106200 then
-          cmd = "ASRB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ASRB', current_word)
         when 0o106300 then
-          cmd = "ASLB\t" + parse_operand(current_word & 0o77)
+          cmd = dd('ASLB', current_word)
         when 0o106400 then
-          cmd = "MTPS\t" + parse_operand(current_word & 0o77)
+          cmd = dd('MTPS', current_word)
         when 0o106700 then
-          cmd = "MFPS\t" + parse_operand(current_word & 0o77)
+          cmd = dd('MFPS', current_word)
         end
       end
     when 0o110000 then
-      cmd = "MOVB\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('MOVB', current_word)
     when 0o120000 then
-      @cmp_step = 1
-      cmd = "CMPB\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('CMPB', current_word)
+      @comparison_status = :comparison_command
     when 0o130000 then
-      cmd = "BITB\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('BITB', current_word)
     when 0o140000 then
-      cmd = "BICB\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('BICB', current_word)
     when 0o150000 then
-      cmd = "BISB\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('BISB', current_word)
     when 0o160000 then
       @cmp_step = 1
-      cmd = "SUB\t" + parse_operand((current_word >> 6) & 0o77) + ',' + parse_operand(current_word & 0o77)
+      cmd = ssdd('SUB', current_word)
     end
 
     # If by now we haven't found a valid command, it must be a constant.
-    cmd = ".#" + current_word.to_s(8) if cmd == ''
+    cmd = '.#' + current_word.to_s(8) if cmd == ''
 
     label = defined?(@labels) && @labels[start_address + command_start_offset]
     @current_offset += 2
-    [ cmd, @current_offset - command_start_offset, newline, label ]
+    @previous_command = cmd
+    [ cmd, @current_offset - command_start_offset, add_newline, label ]
+  end
+
+  def ssdd(cmd, word)
+    cmd + "\t" + parse_operand((word >> 6) & 0o77) + ',' + parse_operand(word & 0o77)
+  end
+
+  def dd(cmd, word)
+    cmd + "\t" + parse_operand(word & 0o77)
+  end
+
+  def rdd(cmd, word)
+    cmd + "\t" + parse_operand((word >> 6) & 0o7) + ',' + parse_operand(word & 0o77)
+  end
+
+  def off(cmd, word)
+    cmd + "\t" + address_or_label(offset2address(word & 0o377))
   end
 
   def parse_operand(code)
@@ -326,22 +331,22 @@ module Disassembler
 
     case (code & 0o70)
     when 0o00 then reg
-    when 0o10 then "(" + reg + ")"
+    when 0o10 then '(' + reg + ')'
     when 0o20 then
       if r == 7 then
         @current_offset += 2
         '#' + word_at(@current_offset).to_s(8)
-      else "(" + reg + ")+"
+      else '(' + reg + ')+'
       end
     when 0o30 then
       if r == 7 then
         @current_offset += 2
         address = word_at(@current_offset)
         '@#' + address_or_label(address)
-      else "@(" + reg + ")+"
+      else '@(' + reg + ')+'
       end
-    when 0o40 then "-(" + reg + ")"
-    when 0o50 then "@-(" + reg + ")"
+    when 0o40 then '-(' + reg + ')'
+    when 0o50 then '@-(' + reg + ')'
     when 0o60 then
       @current_offset += 2
 
@@ -359,7 +364,7 @@ module Disassembler
       offset = word_at(@current_offset)
 
       if r == 7 then
-         address = Tools::rollover(@current_offset + offset + start_address + 2)
+        address = Tools::rollover(@current_offset + offset + start_address + 2)
         '@' + address_or_label(address)
       else
         "@#{offset.to_s(8)}(#{reg})"
@@ -399,4 +404,5 @@ module Disassembler
 
     address.to_s(8)
   end
+
 end
