e64fa36956e5940c05942664ae38fdde62a234ab
weshatheleopard <3384534+weshatheleopard@users.noreply.github.com>
2024-01-11 20:10:58 +0000

    Remove unnecessary context

--- spec/kryo_flux_reader_spec.rb
+++ spec/kryo_flux_reader_spec.rb
@@ -1,23 +1,21 @@
 require 'kryo_flux_reader'
 
 RSpec.describe KryoFluxReader do
-  context KryoFluxReader do
-    let :reader do
-      KryoFluxReader.new "spec/source_data/track.79.0.raw", 0
-    end
+  let :reader do
+    KryoFluxReader.new "spec/source_data/track.79.0.raw", 0
+  end
 
-    let :track do
-      reader.convert_track
-    end
+  let :track do
+    reader.convert_track
+  end
 
-    it "should convert RAW file to TRK file" do        
-      expect(track.fluxes.to_s).to be_same_value_as(:log => 'spec/result_data/track.79.0.fluxes')
-    end
+  it "should convert RAW file to TRK file" do        
+    expect(track.fluxes.to_s).to be_same_value_as(:log => 'spec/result_data/track.79.0.fluxes')
+  end
 
-    it "scan() should detect sync puse length and read track number from the header" do        
-      track.scan
-      expect(track.sync_pulse_length).to eq 79.5
-      expect(track.track_no).to eq 79
-    end
+  it "scan() should detect sync puse length and read track number from the header" do        
+    track.scan
+    expect(track.sync_pulse_length).to eq 79.5
+    expect(track.track_no).to eq 79
   end
 end
